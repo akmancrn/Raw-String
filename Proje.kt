7612c5bee3fe6e5bca22ac7b4c3e70848b96a058
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-14 16:44:04 +0300

    Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+fun main (args : Array<String>)
+{
+   val text="""
+       Bu örnek 
+       Kotlin dilinde
+       Raw String örneğidir
+   """
+   print("Raw String =" +text)
+}
